857ef1f658a2b9057913891516f43b1602a38a5b
mmilica <milica.milutinovic@cs.kuleuven.be>
2015-09-20 16:01:34 +0200

    addingToProvidersDB.pl

---
+++ addingToCollaboratorsDB.pl
@@ -0,0 +1,31 @@
+/*{+==============================================================
+ |
+ | addToCollaboratingProvidersDBs(SP, TransactionID, Attribute, Source)
+ |    inputs: SP, linked Arrtibute and TransactionID
+ |    outputs: ---
+ |    effects: creates a profile entry, a link between a transactionID and an attribute, with the indication of the source
+ |			in the databases of the providers which collaborate with the provider SP		
+ |
+ +================================================================}*/
+
+
+
+addToCollaboratingProvidersDBs(SP, TransactionId, Attribute, Source):-
+write('------------------------------------------'), nl,
+write('IN THE addToCollaboratingProvidersDBs'), nl, 
+write(addToCollaboratingProvidersDBs(SP, TransactionId, Attribute, Source)), nl, 
+	collaboratingSPs(SP, SP1),
+	write('Analysing the DB of collaborating SP: '), write(SP1), nl,
+	(
+		dynamicProfile(SP1, TransactionId, Attribute, Source), !
+	;
+		assert(dynamicProfile(SP1, TransactionId, Attribute, Source)),
+		write('DEBUG: Added to providers database: '), nl, 
+		write(dynamicProfile(SP1, TransactionId, Attribute, Source)), nl
+	), 
+	establishLinks(SP1, TransactionId, Attribute, Source),
+	fail.
+	
+	
+addToCollaboratingProvidersDBs(_, _, _, _).
+
